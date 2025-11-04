dc18f08051c3df78be96b93732d1f7da0eef7e9b
hekengi <118739381+hekengi@users.noreply.github.com>
2025-11-04 21:09:30 +0900

    Title: 사각형 채우기 3, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

--- 251104/사각형 채우기 3/rectangle-fill-3.java
+++ 251104/사각형 채우기 3/rectangle-fill-3.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static int MAX_NUM = 1000;
     public static int MOD = 1000000007;
-    public static int[] arr = new int[MAX_NUM+1];
+    public static long[] dp = new long[MAX_NUM+1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,17 +11,15 @@ public class Main {
         // Please write your code here.
 
         
-        arr[0] = 1;
-        arr[1] = 2;
-        arr[2] = 7;
+        dp[0] = 1;
+        dp[1] = 2;
 
-        for(int i = 3; i <= MAX_NUM; i++){
-            arr[i] = (arr[i-1]*2 + arr[i-2]*3)%MOD;
-            for(int j = i-3; j >= 0; j--){
-                arr[i] = (arr[i] + arr[j]*2)%MOD;
-            }
+        for(int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+            for(int j = i - 3; j >= 0; j--)
+                dp[i] = (dp[i] + dp[j] * 2) % MOD;
         }
         
-        System.out.println(arr[n]);
+        System.out.println(dp[n]);
     }
 }
